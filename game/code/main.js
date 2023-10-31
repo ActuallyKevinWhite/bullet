18c086f9f7dacb9287b765dd03ef4f5f9a7fd9df
kwuite <howiskevinwhite1993taken@gmail.com>
2023-10-30 18:46:18 -0700

    display running

--- game/code/main.js
+++ game/code/main.js
@@ -1,2 +1,22 @@
 // index.ts
+var CONFIG = {
+  DISPLAY_NAME: "display",
+  DISPLAY_WIDTH: 640,
+  DISPLAY_HEIGHT: 480
+};
 console.log("Hello via Bun!");
+var Game = {
+  Canvas: null,
+  Context: null,
+  initialize: function() {
+    this.Canvas = document.getElementById(CONFIG.DISPLAY_NAME);
+    this.Context = this.Canvas.getContext("2d");
+    this.Canvas.width = CONFIG.DISPLAY_WIDTH;
+    this.Canvas.height = CONFIG.DISPLAY_HEIGHT;
+    this.Context.fillStyle = "black";
+    this.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  Game.initialize();
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+const CONFIG = {
+    DISPLAY_NAME: "display",
+    DISPLAY_WIDTH: 640,
+    DISPLAY_HEIGHT: 480,
+}
 console.log("Hello via Bun!");
+
+const Game = {
+    Canvas:  null as HTMLCanvasElement        | null,
+    Context: null as CanvasRenderingContext2D | null,
+
+    initialize: function () {
+        this.Canvas = document.getElementById(CONFIG.DISPLAY_NAME) as HTMLCanvasElement;
+        this.Context = this.Canvas.getContext("2d") as CanvasRenderingContext2D;
+
+        this.Canvas.width = CONFIG.DISPLAY_WIDTH;
+        this.Canvas.height = CONFIG.DISPLAY_HEIGHT;
+
+        this.Context.fillStyle = "black";
+        this.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    Game.initialize();
+});
