4ea8077dbd642f71761c5d08b4a3a2c2aadf87ef
kwuite <howiskevinwhite1993taken@gmail.com>
2023-10-30 18:55:37 -0700

    basic graphics done

--- game/code/main.js
+++ game/code/main.js
@@ -5,16 +5,50 @@ var CONFIG = {
   DISPLAY_HEIGHT: 480
 };
 console.log("Hello via Bun!");
+var Display = {
+  draw_rectangle: function(x, y, w, h, color) {
+    if (!Game.Context) {
+      return false;
+    }
+    Game.Context.fillStyle = color;
+    Game.Context.fillRect(x - w / 2, y - h / 2, w, h);
+    return true;
+  },
+  draw_circle: function(x, y, r, color) {
+    if (!Game.Context) {
+      return false;
+    }
+    Game.Context.fillStyle = color;
+    Game.Context.beginPath();
+    Game.Context.arc(x, y, r, 0, 2 * Math.PI);
+    Game.Context.fill();
+    return true;
+  },
+  draw_line: function(start, end, color) {
+    if (!Game.Context) {
+      return false;
+    }
+    Game.Context.strokeStyle = color;
+    Game.Context.beginPath();
+    Game.Context.moveTo(start.x, start.y);
+    Game.Context.lineTo(end.x, end.y);
+    Game.Context.stroke();
+    return true;
+  }
+};
 var Game = {
   Canvas: null,
   Context: null,
   initialize: function() {
-    this.Canvas = document.getElementById(CONFIG.DISPLAY_NAME);
-    this.Context = this.Canvas.getContext("2d");
-    this.Canvas.width = CONFIG.DISPLAY_WIDTH;
-    this.Canvas.height = CONFIG.DISPLAY_HEIGHT;
-    this.Context.fillStyle = "black";
-    this.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+    Game.Canvas = document.getElementById(CONFIG.DISPLAY_NAME);
+    Game.Context = Game.Canvas.getContext("2d");
+    Game.Canvas.width = CONFIG.DISPLAY_WIDTH;
+    Game.Canvas.height = CONFIG.DISPLAY_HEIGHT;
+    Game.Context.fillStyle = "black";
+    Game.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+    Display.draw_rectangle(100, 100, 32, 32, "red");
+    Display.draw_circle(100, 100, 8, "blue");
+    Display.draw_line({ x: 100, y: 100 }, { x: 200, y: 200 }, "green");
   }
 };
 document.addEventListener("DOMContentLoaded", () => {
--- index.ts
+++ index.ts
@@ -6,20 +6,53 @@ const CONFIG = {
     DISPLAY_HEIGHT: 480,
 }
 console.log("Hello via Bun!");
+interface Vector {
+    x: number,
+    y: number
+}
+const Display = {
+    // Centered for the sake of the bullet hell
+    draw_rectangle: function (x: number, y: number, w: number, h: number, color: string) {
+        if (!Game.Context) { return false; }
+        Game.Context.fillStyle = color;
+        Game.Context.fillRect(x - w / 2, y - h / 2, w, h)
+        return true;
+    },
+    draw_circle: function (x: number, y: number, r: number, color: string) {
+        if (!Game.Context) { return false; }
+        Game.Context.fillStyle = color;
+        Game.Context.beginPath();
+        Game.Context.arc(x, y, r, 0, 2 * Math.PI);
+        Game.Context.fill();
+        return true;
+    },
+    draw_line: function (start: Vector, end: Vector, color: string) {
+        if (!Game.Context) { return false; }
+        Game.Context.strokeStyle = color;
+        Game.Context.beginPath();
+        Game.Context.moveTo(start.x, start.y);
+        Game.Context.lineTo(end.x, end.y);
+        Game.Context.stroke();
+        return true;
+    }
+}
 
 const Game = {
     Canvas:  null as HTMLCanvasElement        | null,
     Context: null as CanvasRenderingContext2D | null,
 
     initialize: function () {
-        this.Canvas = document.getElementById(CONFIG.DISPLAY_NAME) as HTMLCanvasElement;
-        this.Context = this.Canvas.getContext("2d") as CanvasRenderingContext2D;
+        Game.Canvas  = document.getElementById(CONFIG.DISPLAY_NAME) as HTMLCanvasElement;
+        Game.Context = Game.Canvas.getContext("2d")                 as CanvasRenderingContext2D;
 
-        this.Canvas.width = CONFIG.DISPLAY_WIDTH;
-        this.Canvas.height = CONFIG.DISPLAY_HEIGHT;
+        Game.Canvas.width  = CONFIG.DISPLAY_WIDTH;
+        Game.Canvas.height = CONFIG.DISPLAY_HEIGHT;
 
-        this.Context.fillStyle = "black";
-        this.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+        Game.Context.fillStyle = "black";
+        Game.Context.fillRect(0, 0, CONFIG.DISPLAY_WIDTH, CONFIG.DISPLAY_HEIGHT);
+        Display.draw_rectangle(100, 100, 32, 32, 'red')
+        Display.draw_circle(100, 100, 8, 'blue')
+        Display.draw_line({ x: 100, y: 100 }, { x: 200, y: 200 }, 'green')
     }
 }
 
